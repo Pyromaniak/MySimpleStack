2a231baed8ce4fab3b00796897f89b6461c10389
Pyromaniak <lequere.lilian@gmail.com>
2016-02-22 10:56:54 +0100

    commit de la question 3

---
+++ src/main/java/dcll/lleq/Item.java
@@ -0,0 +1,44 @@
+package dcll.lleq;
+
+/**
+ * Created by 21003765 on 17/02/2016.
+ */
+public class Item {
+    public String s;
+
+    public Item(String s) {
+        this.s = s;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    @Override
+    public String toString() {
+        return "dcll.lleq.Item{" +
+                "s='" + s + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (s != null ? !s.equals(item.s) : item.s != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return s != null ? s.hashCode() : 0;
+    }
+}
---
+++ src/main/java/dcll/lleq/ItemTest.java
@@ -0,0 +1,41 @@
+package dcll.lleq;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ItemTest {
+
+    Item item1 = new Item("pomme");
+    Item item2 = new Item("poire");
+
+
+    @Test
+    public void testGetS() throws Exception {
+        Assert.assertEquals(item1.getS(), "pomme");
+        Assert.assertNotEquals(item1.getS(), "fraise");
+    }
+
+    @Test
+    public void testSetS() throws Exception {
+        item1.setS("fraise");
+        Assert.assertEquals(item1.getS(), "fraise");
+        Assert.assertNotEquals(item1.getS(), "pomme");
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        Assert.assertEquals(item1.toString(), "dcll.lleq.Item{" +
+                "s='" + item1.getS() + '\'' +
+                '}');
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+    }
+}
